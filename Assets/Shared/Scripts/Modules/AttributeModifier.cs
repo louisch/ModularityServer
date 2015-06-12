2e4e9d6292fdc6104fc5eabe0fde9701d2e2a245
a virtual fox <anton.alenov12@imperial.ac.uk>
2015-06-12 15:50:17 +0100

    Added ModuleInfo for in game reasons.

---
+++ Assets/Shared/Scripts/Modules/AttributeModifier.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class AttributeModifier {
+	public string attribute;
+	public float modifier;
+
+	public AttributeModifier (string attribute, float modifier)
+	{
+		this.attribute = attribute;
+		this.modifier = modifier;
+	}
+}
---
+++ Assets/Shared/Scripts/Modules/ModuleInfo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ModuleInfo : MonoBehaviour {
+	// ownership information
+	public PhotonPlayer owner;
+
+	// status information
+	public List<AttributeModifier> modifiers = new List<AttributeModifier> ();
+	public float mass;
+
+	// descriptions
+	public string name;
+	public string description;
+	public string actionDescription;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
